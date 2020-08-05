bb15833a7e0d042d63c3e98f37653435779854b6
zackdk <zackvvzackvv@gmail.com>
2020-08-05 15:39:29 +0800

    更改生成文件路径

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import { terser } from "rollup-plugin-terser";
 export default {
     input: 'src/index.js',
     output: {
-      file: 'bundle.js',
+      file: 'dist/mini-gif.min.js',
       name: 'minigif',
       format: 'umd'
     },
